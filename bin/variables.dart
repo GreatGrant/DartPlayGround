304edfedc501720ee71809be7b6a93b170ddc8ce
Great Grant <greatgrant116@gmail.com>
2023-02-09 14:06:26 +0000

    use contant with final keyword

--- bin/variables.dart
+++ bin/variables.dart
@@ -6,11 +6,12 @@ void main() {
   num height = 5;
   bool isAgeEqualHeight = (age == height);
   double shoeSize = 38.2;
+  final noOfEyes = 2;
 
   print(
-      "$name $surname is age $age and $height feet tall.\nHis shoe size is $shoeSize\nFrom this given data, is his age equal to his height? $isAgeEqualHeight");
+      "$name $surname is age $age and $height feet tall. He has $noOfEyes eyes.\nHis shoe size is $shoeSize\nFrom this given data, is his age equal to his height? $isAgeEqualHeight");
 
   print("Does the name $name contains the word 'ant'? ${name.contains("ant")}");
-  
+
   print("shoe size $shoeSize to integer is ${shoeSize.toInt()}");
 }
