d48e6bf42036fe9385346117784da08d4332d4bb
Great Grant <greatgrant116@gmail.com>
2023-03-17 11:41:09 +0000

    create collections.dart

---
+++ bin/collections.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // Lists
+  List<int> ages = [2, 3, 4, 6];
+  List<String> names = ["Ben", "John", "James"];
+  List<dynamic> random = ["something", 56, "another"];
+
+  //Maps
+  Map<String, dynamic> student = {
+    "name": "Grant",
+    "age": 22,
+    "school": "atbu"
+  };
+
+  print("${student["name"]} is ${student["age"]}");
+
+  // Sets
+  Set<int> registrationNumbers = {1, 2, 4,5}
+  ;
+}
