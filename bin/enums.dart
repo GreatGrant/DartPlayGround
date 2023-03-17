290cea8e9de500cf733a8dd846793c3df1bc9492
Great Grant <greatgrant116@gmail.com>
2023-03-17 12:53:36 +0000

    create enum class

---
+++ bin/enums.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  print(AccountType.free.index);
+
+  switch(AccountType.premium){
+    case AccountType.free: print("free");
+    break;
+    case AccountType.premium : print("premium");
+    break;
+    default:
+      print("vip");
+  }
+}
+
+enum AccountType { free, premium, vip}
