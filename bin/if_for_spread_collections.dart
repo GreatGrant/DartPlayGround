3750e1c729e0b5e7c9efd66059aeefbe8d9f1aca
Great Grant <greatgrant116@gmail.com>
2023-03-17 12:32:58 +0000

    create if_for_spread_collections.dart

---
+++ bin/if_for_spread_collections.dart
@@ -0,0 +1,9 @@
+void main(List<String> args){
+  final generatedNumbersList = <int>[for(final number in [1,2,3]) number];
+  print(generatedNumbersList);
+
+  final generatedStringsList = <String>[for(final no in [1,3, 4]) no.toString()];
+  print(generatedStringsList);
+
+
+}
