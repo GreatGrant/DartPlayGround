72315e185c251c9f199f4ca06be37f11578b71ca
Great Grant <greatgrant116@gmail.com>
2023-03-20 19:20:01 +0000

    class prperties.dart

--- bin/.vscode/private_test.dart
+++ bin/.vscode/private_test.dart
@@ -1,8 +1,8 @@
 import 'private_fields.dart';
 void main(){
   final x = Student("Grant", 2);
-
+  final y = Student.namedConstructor(name: "Ben", id: 3);
   // the lines below do not work because they are private fields
   // x._name = "ben";
-  // print(x._name);
+  // print(y._name);
 }
